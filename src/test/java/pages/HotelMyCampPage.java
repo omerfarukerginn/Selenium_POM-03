7d54f569a07094579c816643ac34a89c13f3d9d0
Ömer Faruk Ergin <omerfarukerginn@gmail.com>
2022-02-07 15:07:38 +0300

    day 17

--- src/test/java/pages/HotelMyCampPage.java
+++ src/test/java/pages/HotelMyCampPage.java
@@ -22,4 +22,19 @@ public class HotelMyCampPage {
     @FindBy(xpath = "//input[@id='btnSubmit']")
     public WebElement loginButonu;
 
+    @FindBy(xpath = "//div[@class='validation-summary-errors']")
+    public WebElement girisYapilamadiYaziElementi;
+
+    @FindBy(xpath = "//span[text()='ListOfUsers']")
+    public WebElement basariliGirisYaziElementi;
+
+
+    public void bekle(int saniye){
+     try {
+      Thread.sleep(saniye*1000);
+     } catch (InterruptedException e) {
+      e.printStackTrace();
+     }
+    }
+
 }
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,21 +3,47 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 import java.time.Duration;
 
 public class Driver {
 
+    private Driver(){
+
+    }
+
     public static WebDriver driver;
 
     public static WebDriver getDriver(){
 
-        WebDriverManager.chromedriver().setup();
 
         if (driver==null){ // bu if sayesinde kod calisirken bir kere new keyword ile driver olusturulacak
                            // diger kullanimlarda new devreye girmeyecek
 
-            driver=new ChromeDriver();
+            switch (ConfigReader.getProperty("browser")){
+
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    break;
+                case "opera":
+                    WebDriverManager.operadriver().setup();
+                    driver=new OperaDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver=new EdgeDriver();
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver();
+            }
 
         }
         driver.manage().window().maximize();
