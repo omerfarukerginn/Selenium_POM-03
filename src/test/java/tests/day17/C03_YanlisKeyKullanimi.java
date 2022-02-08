85baf7467a5f5cc6437a807b37a7eb7f5c6ae5f4
Ömer Faruk Ergin <omerfarukerginn@gmail.com>
2022-02-09 00:33:30 +0300

    day21

--- src/test/java/tests/day17/C03_YanlisKeyKullanimi.java
+++ src/test/java/tests/day17/C03_YanlisKeyKullanimi.java
@@ -8,7 +8,7 @@ public class C03_YanlisKeyKullanimi {
 
     @Test
     public void test01(){
-        Driver.getDriver().get(ConfigReader.getProperty("HMCurl"));
+        Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
         // eger key olarak girdigimiz String configuration.properties dosyasinda yoksa
         // Ornegin HMCUrl yerine HMCurl yazarsak,
         // ConfigReader.getProperty() o key'i bulamaz
